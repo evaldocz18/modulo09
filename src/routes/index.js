527f366dee3de1911e5d5f51486ba104f7337281
Evaldo <evaldocz18@gmail.com>
2020-08-05 15:54:58 -0300

    Rotas Privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-
-import Dahsboard from '../pages/Dashboard';
+import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 
 export default function Routes() {
@@ -13,8 +13,10 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dahsboard} />
+      <Route path="/dashboard" component={Dashboard} />
       <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
